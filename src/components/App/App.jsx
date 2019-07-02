443731d2e99d8af995c518076c156ef29a8df60a
Max Silver <Maxbsilver@gmail.com>
2019-07-02 13:53:44 -0600

    add index import to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "../../scss/index.scss";
 
 function App() {
-  return <div className="App" />;
+  return <div className="App">hello</div>;
 }
 
 export default App;
