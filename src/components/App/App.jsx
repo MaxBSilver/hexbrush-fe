cd268d3da369a70252dcdbf13803e4e405cf9008
Jarrett Kong <jarrettkong@me.com>
2019-07-02 19:56:07 -0600

    Query projects and palettes from state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
-import "../../scss/index.scss";
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="App">hello</div>;
+class App extends Component {
+	state = {
+		loading: false,
+		projects: [],
+		palettes: []
+	};
+
+	componentDidMount() {
+		this.setState({ loading: true }, async () => {
+			const projectsRes = await fetch('http://localhost:3001/api/v1/projects');
+			const palettesRes = await fetch('http://localhost:3001/api/v1/palettes');
+			const projects = await projectsRes.json();
+			const palettes = await palettesRes.json();
+			this.setState({ loading: false, projects, palettes });
+		});
+	}
+
+	render() {
+		return <div />;
+	}
 }
 
 export default App;
