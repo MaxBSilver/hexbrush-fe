4dd6bc1b5ea37ad1c8a5c57d8dd8b818324834bd
Max Silver <Maxbsilver@gmail.com>
2019-07-07 13:20:16 -0600

    add palette-button class identifier and move html

--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import PaletteColor from '../PaletteColor/PaletteColor';
 
 const Palette = props => {
-	const { color_1, color_2, color_3, color_4, color_5}= props;
-	const hexCodes = [color_1, color_2, color_3, color_4, color_5];
+	const { color_1, color_2, color_3, color_4, color_5 } = props;
+	const hexCodes = [ color_1, color_2, color_3, color_4, color_5 ];
 	return (
 		<article className="Palette">
 			<div className="Palette-header">
 				<h3 className="Palette-title">{props.name || 'Untitled Palette'}</h3>
-				<div className="Palette-teardrop-container">
-					<PaletteColor hexcode={color_1} />
-					<PaletteColor hexcode={color_2} />
-					<PaletteColor hexcode={color_3} />
-					<PaletteColor hexcode={color_4} />
-					<PaletteColor hexcode={color_5} />
-				</div>
+			</div>
+
+			<div className="Palette-teardrop-container">
+				<PaletteColor hexcode={color_1} />
+				<PaletteColor hexcode={color_2} />
+				<PaletteColor hexcode={color_3} />
+				<PaletteColor hexcode={color_4} />
+				<PaletteColor hexcode={color_5} />
 			</div>
 			<div className="Palette-button-container">
-				<i className="material-icons" role="button" onClick={() => props.addEditState(hexCodes)}>
+				<i className="material-icons Palette-button" role="button" onClick={() => props.addEditState(hexCodes)}>
 					edit
 				</i>
 				<i className="material-icons Palette-button" role="button" onClick={() => props.deletePalette(props.id)}>
