627a05ddf2dab9f7689e7eda7de23e0b1aabbdd3
Jarrett Kong <jarrettkong@me.com>
2019-07-08 17:01:50 -0600

    Add missing palette tests

--- src/components/Palette/Palette.test.jsx
+++ src/components/Palette/Palette.test.jsx
@@ -3,6 +3,8 @@ import Palette from './Palette';
 import { shallow } from 'enzyme';
 
 const mockPalette = {
+	id: 99,
+	project_id: 3,
 	color_1: '#899e01',
 	color_2: '#ff21c3',
 	color_3: '#88b712',
@@ -11,12 +13,26 @@ const mockPalette = {
 };
 
 describe('Palette', () => {
-	let wrapper;
+	let wrapper, mockAddEditState, mockDeletePalette;
 	beforeEach(() => {
-		wrapper = shallow(<Palette {...mockPalette} />);
+		mockAddEditState = jest.fn();
+		mockDeletePalette = jest.fn();
+		wrapper = shallow(<Palette {...mockPalette} addEditState={mockAddEditState} deletePalette={mockDeletePalette} />);
 	});
 
 	it('should match the snapshot', () => {
 		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should call addEditState on edit button click with hexcodes', () => {
+		const { color_1, color_2, color_3, color_4, color_5 } = mockPalette;
+		const mockHexCodes = [color_1, color_2, color_3, color_4, color_5];
+		wrapper.find('.edit-btn').simulate('click');
+		expect(mockAddEditState).toHaveBeenCalledWith(mockHexCodes);
+	});
+
+	it('should call deletePalette on delete click with id', () => {
+		wrapper.find('.delete-btn').simulate('click');
+		expect(mockDeletePalette).toHaveBeenCalledWith(mockPalette.id);
 	});
 });
