1582ff6395c162fb81bba19cae675f5435686605
Jarrett Kong <jarrettkong@me.com>
2019-07-09 14:35:59 -0600

    Add palette rename form

--- src/components/PaletteView/PaletteView.jsx
+++ src/components/PaletteView/PaletteView.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from '../Palette/Palette';
 
-const PaletteView = props => {
-	const { palettes, addEditState, deletePalette, selectedProject, projects } = props;
-	return (
-		<React.Fragment>
-			<select
-				className="App-project-select palette-view-select"
-				value={selectedProject}
-				onChange={e => props.selectProject(e.target.value)}>
-				<option value="0">--</option>
-				{projects.map(p => (
-					<option key={p.id} value={p.id}>
-						{p.name}
-					</option>
-				))}
-			</select>
-			<output className="PaletteView">
-				{palettes.map(palette => (
-					<Palette key={palette.id} deletePalette={deletePalette} addEditState={addEditState} {...palette} />
-				))}
-			</output>
-		</React.Fragment>
-	);
-};
+class PaletteView extends Component {
+	state = {
+		projectName: '',
+	};
+
+	render() {
+		const { palettes, addEditState, deletePalette, selectedProject, projects } = this.props;
+		return (
+			<React.Fragment>
+				<form>
+				<select
+					className="App-project-select palette-view-select"
+					value={selectedProject}
+					onChange={e => this.props.selectProject(e.target.value)}
+				>
+					<option value="0">--</option>
+					{projects.map(p => (
+						<option key={p.id} value={p.id}>
+							{p.name}
+						</option>
+					))}
+				</select>
+					<input
+						type="text"
+						value={this.state.projectName}
+						onChange={e => this.setState({ paletteName: e.target.value })}
+					/>
+					<button id="PaletteView-edit-project-btn">Edit Project Name</button>
+					<button id="PaletteView-delete-project-btn">Delete Project</button>
+				</form>
+				<output className="PaletteView">
+					{palettes.map(palette => (
+						<Palette key={palette.id} deletePalette={deletePalette} addEditState={addEditState} {...palette} />
+					))}
+				</output>
+			</React.Fragment>
+		);
+	}
+}
 
 export default PaletteView;
